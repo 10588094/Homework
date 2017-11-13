02a64e8c8274755e91c68fffab42a0912a6a910d
[Daph] <[daphnelaura_@hotmail.com]>
2017-11-13 14:20:09 +0100

    heuy

---
+++ deEchteWeek3/convertCSV2JSON.py
@@ -0,0 +1,5 @@
+# VERGEET HEADER NIET
+# wat voor format moet de JSON data worden?
+# is knmi nu csv file? (install plugins?)
+# werkt versie 3 van D3 in python 3? of moet ik pyton 2.7 gebruiken?
+# bron vermelding op website -> link nog beter
