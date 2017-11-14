956bfd095a75524a43d485a3907f295a7dc292b4
[Daph] <[daphnelaura_@hotmail.com]>
2017-11-14 19:03:38 +0100

    files

--- deEchteWeek3/convertCSV2JSON.py
+++ deEchteWeek3/convertCSV2JSON.py
@@ -3,3 +3,18 @@
 # is knmi nu csv file? (install plugins?)
 # werkt versie 3 van D3 in python 3? of moet ik pyton 2.7 gebruiken?
 # bron vermelding op website -> link nog beter
+
+import json
+import csv
+
+f = open("KNMI_2015.txt", "r")
+n = json.dumps(f)
+o = json.loads(n)
+# r = f.read()
+# #print (type(r))
+# j = json.dumps("r")
+# print(j)
+
+# data
+with open('KNMI_2015.csv', 'r') as outfile:
+    reader = csv.reader(line.replace)
